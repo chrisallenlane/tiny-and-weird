e040d013c24e3bd94be2ecd21af2e31a1142722e
Chris Lane <chris@chris-allen-lane.com>
2013-01-01 15:47:00 -0500

    Reconfigured the minifier example script to strip whitespace.

--- minify.php
+++ minify.php
@@ -6,7 +6,7 @@ include './tiny-and-weird.php';
 
 # set some minifier options
 $options = array(
-    'remove_whitespace' => false,
+    'remove_whitespace' => true,
     'tokens_to_ignore'  => array(
         'alpha', 'bravo', 'charlie',
     ),
